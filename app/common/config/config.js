a8fd60942890c24c6f179fb27a80fda21f3cfa2b
xin <xinsngx@163.com>
2016-12-14 15:10:54 +0800

    UPDATE AND TIME START

---
+++ app/common/config/config.js
@@ -0,0 +1,11 @@
+'use strict';
+/**
+ * config
+ */
+
+exports.__esModule = true;
+exports.default = {
+  //key: value
+  port: 8888
+};
+//# sourceMappingURL=config.js.map
--- js/responsive.js
+++
@@ -1,7 +0,0 @@
-$(window).resize(function(){	
-	if ($("body").css("padding") == "10px" ){
-		// your code here
-		$("#skin-select #toggle.active").css({"display":"none"});
-
-	}
-});
